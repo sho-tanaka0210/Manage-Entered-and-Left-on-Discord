438eb2c5cc4ca635c41e337b43269f4c8616462a
Mizukichi0210 <canaryyellow.nl@gmail.com>
2019-03-22 23:52:03 +0900

    Improved bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 import discord
 import configparser
+import re
 
 client = discord.Client()
 
@@ -16,33 +17,39 @@ SEPARATOR = "\n"
 # If someone entered '!help'.
 @client.event
 async def on_message(message):
-    if message.content.startswith('!help'):
-        text = inifile.get('message','help_message_1') + SEPARATOR + inifile.get('message','help_message_2')
-        await client.send_message(message.channel, text)
+	if message.content.startswith('!help'):
+		text = inifile.get('message','help_message_1') + SEPARATOR + inifile.get('message','help_message_2')
+		await client.send_message(message.channel, text)
 
 # 入退室管理
 # Manage Entered and left.
 @client.event
 async def on_voice_state_update(before, after):
-    player_name = ""
-    text = ""
+	player_name = ""
+	text = ""
 
-    # 入室した場合
-    # If someone enterd VOICE CHANNEL.
-    if(before.voice_channel is None):
-        player_name = after.name
-        text = player_name + inifile.get('message','entering_message')
+	channel_name = inifile.get('bot_settings', 'channel_name')
 
-    # 退室した場合
-    # If someone left VOICE CHANNEL.
-    elif(after.voice_channel is None):
-        player_name = before.name
-        text = player_name + inifile.get('message','leaving_message')
-    
-    # 発言するチャンネルの指定
-    # Set channel id.
-    channel = client.get_channel(inifile.get('bot_settings','channel_id'))
-    await client.send_message(channel, text)
+	# config.ini へ記載したチャンネル名と一致した場合
+	if(channel_name == str(before.voice_channel)
+			or channel_name == str(after.voice_channel)):
+
+		# 入室した場合
+		# If someone enterd VOICE CHANNEL.
+		if(before.voice_channel is None):
+			player_name = after.name
+			text = player_name + inifile.get('message','entering_message')
+
+		# 退室した場合
+		# If someone left VOICE CHANNEL.
+		elif(after.voice_channel is None):
+			player_name = before.name
+			text = player_name + inifile.get('message','leaving_message')
+
+		# 発言するチャンネルの指定
+		# Set channel id.
+		channel = client.get_channel(inifile.get('bot_settings','channel_id'))
+		await client.send_message(channel, text)
 
 # botの接続と起動
 # Set token.
