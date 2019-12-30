7d8c2c71e5951f0fd17782970544171399a7e00b
Mizukichi0210 <canaryyellow.nl@gmail.com>
2019-12-30 20:05:49 +0900

    Add constants and modify config_sample.ini

---
+++ constants.py
@@ -0,0 +1,21 @@
+import const
+
+# メッセージ固定値
+# botが起動するときにherokuへ表示するメッセージ
+const.RUN_MESSAGE = 'bot start'
+const.IN = '[IN→] '
+const.OUT = '[←OUT] '
+# const.ENTERING_MESSAGE = 'さんが入室しました。'
+const.ENTERING_MESSAGE = 'is entered.'
+# const.LEAVING_MESSAGE = 'さんが退室しました。'
+const.LEAVING_MESSAGE = 'is left.'
+# const.POST_MESSAGE = '定期的に投稿したいメッセージ'
+const.POST_MESSAGE = 'Message'
+
+# メッセージ装飾用
+# 改行コード
+const.SEPARATOR = '\n'
+# メッセージの強調用
+const.CODEBLOCKS = '```'
+# @everyone メンション
+const.EVERYONE = '@everyone'
