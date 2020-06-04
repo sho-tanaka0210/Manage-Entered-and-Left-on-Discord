e9229af927ce205c765fe5562dd3c39507804ed5
Mizukichi0210 <canaryyellow.nl@gmail.com>
2020-06-04 23:58:44 +0900

    Update contants.py

--- constants.py
+++ constants.py
@@ -6,11 +6,9 @@ const.RUN_MESSAGE = 'bot start'
 const.IN = '[IN→] '
 const.OUT = '[←OUT] '
 # const.ENTERING_MESSAGE = 'さんが入室しました。'
-const.ENTERING_MESSAGE = 'is entered.'
+const.ENTERING_MESSAGE = ' has entered the voice chat.'
 # const.LEAVING_MESSAGE = 'さんが退室しました。'
-const.LEAVING_MESSAGE = 'is left.'
-# const.POST_MESSAGE = '定期的に投稿したいメッセージ'
-const.POST_MESSAGE = 'Message'
+const.LEAVING_MESSAGE = ' has left the voice chat.'
 
 # メッセージ装飾用
 # 改行コード
